99c2cadf9df5cfff211d87f238c14b6c2d406d7f
solarTribute <solartribute@gmail.com>
2022-08-26 13:14:53 +0300

    Commit before Ubuntu install xd

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,35 +1,22 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-//const API_URL = 'http://localhost:8080/api/test/';
-const API_URL = '';
+const API_URL = 'http://localhost:8080/api/';
 
 class UserService {
   getPublicContent() {
-    if (API_URL === '') {
-      return new Promise(() => "API_URL not defined");
-    }
     return axios.get(API_URL + 'all');
   }
 
   getUserBoard() {
-    if (API_URL === '') {
-      return new Promise(() => "API_URL not defined");
-    }
     return axios.get(API_URL + 'user', { headers: authHeader() });
   }
 
   getModeratorBoard() {
-    if (API_URL === '') {
-      return new Promise(() => "API_URL not defined");
-    }
     return axios.get(API_URL + 'mod', { headers: authHeader() });
   }
 
   getAdminBoard() {
-    if (API_URL === '') {
-      return new Promise(() => "API_URL not defined");
-    }
     return axios.get(API_URL + 'admin', { headers: authHeader() });
   }
 }
